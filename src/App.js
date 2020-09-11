e685693c9fbef908af10e20b3e75fa099f92fe1b
Robert-Rodriguez <robertmasters@hotmail.com>
2020-09-11 09:41:18 -0700

    finished axios call, just need to style

--- src/App.js
+++ src/App.js
@@ -1,17 +1,43 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
-
+import Character from './components/Character'
+import axios from 'axios'
+import { BASE_URL } from '../src/index'
+import styled from 'styled-components'
 const App = () => {
+
+  const [characters, setCharacters] = useState([])
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+  useEffect(() => {
+    axios.get(`${BASE_URL}`)
+    .then(good => {
+      setCharacters(good.data.results)
+    })
+    .catch(error => {
+      console.log(error)
+      debugger
+    })
+  }, [])
+
 
   return (
-    <div className="App">
-      <h1 className="Header">Characters</h1>
+    <div>
+      <div className="App">
+        <h1 className="Header">Characters</h1>
+      </div>
+
+      <div>
+        {
+          characters.map((info, i ) => (
+            <Character key = {i} info = {info} />
+          ))
+        }
+      </div>
     </div>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,24 @@
 // Write your Character component here
+import React from 'react'
+import styled from 'styled-components';
+
+
+const Character = props => {
+    const { info } = props
+
+    return (
+
+        <div className ='character-component'>
+
+            <h4>Name: { info.name }</h4>
+            <div>
+
+                <div className = 'character-information'>BirthDate: {info.birth_year} </div>
+                <div className = 'character-information'>Gender: {info.gender} </div>
+                
+            </div>
+        </div>
+    )
+}
+
+export default Character
